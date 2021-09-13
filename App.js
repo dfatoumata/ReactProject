6382ad5f330834993c18d96d5e6a75ae6b00194a
asmaa <bennassefasmaa@gmail.com>
2021-09-13 12:03:22 +0200

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test application!</Text>
+      <Text>test asmaa</Text>
       <StatusBar style="auto" />
     </View>
   );
