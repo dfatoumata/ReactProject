5988c311f6232112f8b3f7e873ceff7c10dbc230
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-14 12:21:02 +0200

    init favoris Suppression

--- src/pages/FavorisSuppression.js
+++ src/pages/FavorisSuppression.js
@@ -3,11 +3,59 @@ import { Text, View } from "react-native";
 
 const FavorisSuppression = () => {
   return (
-    <View>
-      FavorisSuppression
-      <Text>FavorisSuppression</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.textTitle}>Êtes vous sur de vouloir supprimer cette recette de vos favoris </Text>
+      <Text style = {styles.textContent}> Poulet sauce moutarde </Text>
+
+      <button style = {styles.buttonOui}> Oui</button>
+      <button style = {styles.buttonNon}> Non</button>
+      
     </View>
   );
 };
+
+
+export const styles = StyleSheet.create({
+  container: {
+    backGroundColor: '#EBEAEB', 
+  },
+  textTitle: {   
+    color: '#70716F',
+    margin: 5,
+    padding: 15,
+    fontSize: 20,
+  },
+  textContent: {    
+    color: '#70716F',
+    margin: 5,
+    padding: 15,
+    fontSize: 16,
+  },
+  buttonOui: {
+    color: '#C55252',
+    fontSize: 16,
+    textColor: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 15,
+    marginTop: 8,
+    marginBottom: 10,
+  },
+  buttonNon: {
+    color: '#688649',
+    fontSize: 16,
+    textColor: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 15,
+    marginTop: 8,
+    marginBottom: 10,
+  },
+  error: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'red',
+  },
+})
 
 export default FavorisSuppression;
