3a66d3e44991c25c20cc18de2f17a9a929f09d9b
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-14 14:10:19 +0200

    update2

--- src/pages/Bienvenue.js
+++ src/pages/Bienvenue.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
-const Bienvenue = () => {
+import { ImageBackground, Text, View, } from "react-native";
+export default Bienvenue = () => {
   return (
-    <View>
-      Bienvenue
+    <View >
+      <ImageBackground source={image} style={styles.imageB} resizeMode="cover" />
       <Text>Bienvenue</Text>
     </View>
   );
 };
 
-export default Bienvenue;
+export const styles = StyleSheet.create({
+  container: {
+    backGroundColor: '#EBEAEB', 
+
+  },
+  textTitle: {   
+    color: '#70716F',
+    margin: 5,
+    padding: 15,
+    fontSize: 20,
+  },
+  imageB: {
+    flex: 1,
+    justifyContent: "center"
+  },
+})
