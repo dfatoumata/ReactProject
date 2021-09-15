f6802584a8e4f89c8347497540040ad5fb28c5cb
Fatoumata DIARRA <dfatoumata57@yahoo.fr>
2021-09-15 09:29:02 +0200

    Validation BE firebase

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,74 +1,116 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { Link } from "react-router-native";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Accueil from "../pages/Accueil";
+import Inscription from "../pages/Inscription";
+import AjouterRecette from "../pages/AjouterRecette";
+import Favoris from "../pages/Favoris";
+import Profil from "../pages/Profil";
+import HomeSvg from "../../assets/home";
+import LogoSvg from "../../assets/logo";
+import AjouterSvg from "../../assets/ajouter";
+import FavorisSvg from "../../assets/favoris";
+import ProfilSvg from "../../assets/profil";
+const Tab = createBottomTabNavigator();
+import Svg, { Path } from "react-native-svg";
 const FooterMenu = () => {
   return (
-    <View style={stylesMenu.container}>
-      <Link to="/">
-        <Image
-          style={stylesMenu.Image}
-          source={require("../../assets/home.svg")}
-        />
-      </Link>
-      <Link to="/">
-        <Image
-          style={stylesMenu.Image}
-          source={require("../../assets/ajouter.svg")}
-        />
-      </Link>
-      <Link to="/">
-        <Image
-          style={stylesMenu.Image}
-          source={require("../../assets/logo.svg")}
-        />
-      </Link>
-      <Link to="/">
-        <Image
-          style={stylesMenu.Image}
-          source={require("../../assets/favoris.svg")}
-        />
-      </Link>
-      <Link to="/connexion">
-        <Image
-          style={stylesMenu.Image}
-          source={require("../../assets/profil.svg")}
-        />
-      </Link>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Accueil"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarActiveBackgroundColor: "#70716F",
+        tabBarInactiveTintColor: "#70716F",
+        tabBarInactiveBackgroundColor: "white",
+      }}
+    >
+      <Tab.Screen
+        name="Accueil"
+        component={Accueil}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <Svg width="35" height="30">
+              <Path d={HomeSvg} fill={color} />
+            </Svg>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AjouterRecette"
+        component={AjouterRecette}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <Svg width="35" height="30">
+              <Path d={AjouterSvg} fill={color} width="60" />
+            </Svg>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Inscription"
+        component={Inscription}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <Svg width="80" height="150">
+              <Path d={LogoSvg} fill={color} />
+            </Svg>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favoris"
+        component={Favoris}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <Svg width="35" height="30">
+              <Path d={FavorisSvg} fill={color} />
+            </Svg>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profil"
+        component={Profil}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color }) => (
+            <Svg width="35" height="30">
+              <Path d={ProfilSvg} fill={color} />
+            </Svg>
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
 export default FooterMenu;
 const stylesMenu = StyleSheet.create({
   container: {
-    position: 'fixed',
-    bottom: 0,
+    position: "fixed",
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     flexDirection: "row",
-    width: '100%',
-    minHeight: 100,
+    width: "100%",
+    minHeight: 60,
     backgroundColor: "#EBEAEB",
+    alignSelf: "center",
     backdropFilter: blur(4),
     /* Note: backdrop-filter has minimal browser support */
   },
 
   Image: {
-    width: 44,
-    height: 45,
-    left: 13,
-    top: 12,
-    margin: 15,
-    fontFamily: "Awesome",
-    fontStyle: "normal",
-    fontWeight: 900,
-    fontSize: 30,
-    lineHeight: 34,
-
+    minWidth: 40,
+    minHeight: 50,
+    // lineHeight: 34,
+    alignSelf: "center",
     alignItems: "center",
     textAlign: "center",
-
     color: "#70716F",
   },
 });
--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { Redirect } from "react-router-native";
+import { auth } from "../components/firebase";
 
 const Inscription = () => {
+  const SENDING_STEPS = {
+    NONE: "NONE",
+    VALIDATION: "VALIDATION",
+    SEND_DATA: "SEND_DATA",
+  };
+  const [emailField, setEmailField] = useState({ value: "", error: "" });
+  const [passwordField, setPasswordField] = useState({ value: "", error: "" });
+  const [confirmPasswordField, setConfirmPasswordField] = useState({
+    value: "",
+    error: "",
+  });
+  const [formError, setFormError] = useState("");
+  const [step, setStep] = useState(SENDING_STEPS.NONE);
+  useEffect(() => {
+    if (step !== SENDING_STEPS.VALIDATION) return;
+    const validateEmail = () => {
+      let valid = true;
+      if (!emailField.value) {
+        setEmailField({
+          value: emailField.value,
+          error: "Vous devez renseigner un email",
+        });
+        valid = false;
+      }
+      if (!/^[^@]+@[^.]+\.[a-zA-Z0-9]+/.test(emailField.value)) {
+        setEmailField({
+          value: emailField.value,
+          error: "Vous devez renseigner un email valide",
+        });
+        valid = false;
+      }
+      return valid;
+    };
+    const validatePasswords = () => {
+      let valid = true;
+      if (!passwordField.value) {
+        setPasswordField({
+          value: passwordField.value,
+          error: "Vous devez renseigner un mot de passe",
+        });
+        valid = false;
+      }
+      if (!confirmPasswordField.value) {
+        setConfirmPasswordField({
+          value: confirmPasswordField.value,
+          error: "Vous devez répéter le mot de passe",
+        });
+        valid = false;
+      }
+      if (passwordField.value !== confirmPasswordField.value) {
+        setFormError("Vos deux mots de passe doivent correspondre");
+        valid = false;
+      }
+      return valid;
+    };
+    setFormError("");
+    let validEmail = validateEmail();
+    let validPassword = validatePasswords();
+    if (validEmail && validPassword) {
+      setStep(SENDING_STEPS.SEND_DATA);
+    } else {
+      setStep(SENDING_STEPS.NONE);
+    }
+  }, [step]);
+
+  useEffect(() => {
+    if (step !== SENDING_STEPS.SEND_DATA) return;
+    const sendToFirebase = async () => {
+      try {
+        const userCredential = await auth.createUserWithEmailAndPassword(
+          emailField.value,
+          passwordField.value
+        );
+        console.warn(userCredential);
+        setStep(SENDING_STEPS.NONE);
+        if (userCredential.user.email) return <Redirect to="/connexion" />;
+      } catch (e) {
+        setFormError(e.message);
+        setStep(SENDING_STEPS.NONE);
+      }
+    };
+    sendToFirebase();
+  }, [step]);
+
+  const changeField = (setField) => (value) =>
+    setField({
+      error: "",
+      value,
+    });
   return (
     <View>
       Inscription
