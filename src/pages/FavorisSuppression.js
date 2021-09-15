e381da154327a26a4eb012042c14a7240e0d65cd
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-15 12:36:59 +0200

    changeCSS

--- src/pages/FavorisSuppression.js
+++ src/pages/FavorisSuppression.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { Button, ImageBackground, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 
-const image = {
+const image1 = {
   uri:
     'https://images-ext-2.discordapp.net/external/JnnY8hLdRXYOhZTGvHys81oWpKtrFe-1rIb-34vqvKc/%3Falt%3Dmedia%26token%3D07109fee-00af-4d9a-a816-a35d6736e306/https/firebasestorage.googleapis.com/v0/b/cookthat-8cd9b.appspot.com/o/Suhis.jpg?width=400&height=267',
 }
@@ -12,44 +12,43 @@ const onPressNon = () => {}
 
 export default () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={image}
-        style={styles.imageB}
-        resizeMode="cover"
-      />
-      
-
-      <View><Text style={styles.textTitle}>
+   // <ScrollView >
+    
+      <View style={styles.container}>
+      <ImageBackground source={image1} style={styles.imageBg} resizeMode="cover" />
+     
+      <View>
+      <Text style={styles.textTitle}>
           Êtes vous sur de vouloir supprimer cette recette de vos favoris{' '}
         </Text>
+      <Text style={styles.textContent}> Poulet sauce moutarde </Text>
         </View>
-        <View>
-        <Text style={styles.textContent}> Poulet sauce moutarde </Text>
-        </View>
-        <View>
-        <Button
+        <View style={styles.button}>
+        <TouchableOpacity
+          style={styles.btnStyle}
           color="#C55252"
-          style={styles.buttonOui}
+         // style={styles.button}
           title="Oui"
           onPress={onPressOui}>
-        </Button>
-
-        
+          <Text style={styles.whiteText}>OUI</Text>
+        </TouchableOpacity>
         <Button
+          style={styles.btnStyle}
           color="#688649"
-          style={styles.buttonNon}
+         
           title="Non"
           onPress={onPressNon}
         ></Button>
+     </View>
       </View>
-    </View>
+    //</ScrollView>
+    
   )
 }
 
 export const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundImage: '#B8AE57'
   }, 
   textTitle: {
@@ -66,32 +65,32 @@ export const styles = StyleSheet.create({
     padding: 15,
     fontSize: 16,
   },
-  buttonOui: {
+  button: {
+    justifyContent: 'space-between',
     fontSize: 16,
     textColor: 'white',
     fontWeight: 'bold',
-    textAlign: 'center',
+    //textAlign: 'center',
     padding: 15,
+    margin: 5,
     marginTop: 8,
     marginBottom: 10,
+    flexDirection: 'row'
   },
-
-  buttonNon: {
-    fontSize: 16,
-    textColor: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+  btnStyle: {
+    flex: 1,
+    flexBasis: 100,
+    backgroundColor: 'red',
     padding: 15,
-    marginTop: 8,
-    marginBottom: 10,
+    borderRadius: 10,
   },
-  imageB: {
+  imageBg: {
     flex: 1,
     justifyContent: 'center',
     width: '100%',
     height: '100%',
     margin: 5,
-    position: 'relative',
+    position: 'absolute',
     top: 0,
     left: 0,
     opacity: '0.5',
@@ -101,4 +100,8 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'red',
   },
+  whiteText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 })
